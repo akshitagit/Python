13fae96b6453b311da078d7cfb5267d4dd08761d
Kabir Nagpal <21kabirnagpal@gmail.com>
2020-09-26 01:14:43 +0530

    Update string_compression.py
    Solution updated

--- Strings/string_compression.py
+++ Strings/string_compression.py
@@ -1,11 +1,17 @@
 strin = str(input("enter a string: "))
 strarr = dict()
 strout = str()
-for a in strin:
-	if a in strarr.keys():
-		strarr[a]+=1
-	else:
-		strarr[a]=1
-for b in strarr.keys():
-	strout += b+str(strarr[b])
+i = -1
+while(i<len(strin)-1):
+    i+=1
+    ch = strin[i]
+    count = 0
+    for c in range(i,len(strin)):
+        if strin[c]!=ch:
+            c-=1
+            break
+        else:
+            count+=1
+    strout+= ch + str(count)
+    i = c
 print(strout)
