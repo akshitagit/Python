440b2599fc52b8457c1d912d33e17b08e42976a9
Kartik patil <kartikpatil1997@gmail.com>
2020-09-25 16:46:39 +0530

    Adding String compression algorithm

---
+++ Strings/string_compression.py
@@ -0,0 +1,29 @@
+# code for string compression 
+
+def compress(string):
+
+    res = ""
+
+    count = 1
+
+    #Add in first character
+    res += string[0]
+
+    #Iterate through loop, skipping last one
+    for i in range(len(string)-1):
+        if(string[i] == string[i+1]):
+            count+=1
+        else:
+            if(count > 1):
+                #Ignore if no repeats
+                res += str(count)
+            res += string[i+1]
+            count = 1
+    #print last one
+    if(count > 1):
+        res += str(count)
+    return res
+
+if __name__ == "__main__":
+    string=input()
+    print(compress(string))
