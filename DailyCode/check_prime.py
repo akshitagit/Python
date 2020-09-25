2b6e24da12ea5869b0cb0bca7b1e54da088c5c55
Sameera Lakshitha <lakshithasameera89@gmail.com>
2020-09-25 10:02:39 +0530

    Create check_prime.py

---
+++ DailyCode/check_prime.py
@@ -0,0 +1,18 @@
+# Program to check if a number is prime or not
+
+#input from the user
+num = int(input("Enter a number: "))
+
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"It's not a prime number")
+           break
+   else:
+       print(num,"It's a prime number")
+       
+# if input number is less than or equal to 1
+else:
+   print(num,"It's not a prime number")
