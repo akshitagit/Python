94d8c3231f4e0c6a71c23936e74bd6cbc85c823f
Adel Bordbari <bordbariadel@gmail.com>
2020-09-26 06:50:54 +0330

    update anagram_checker.py

--- Strings/anagram_checker.py
+++ Strings/anagram_checker.py
@@ -1,14 +1,12 @@
 def check(a, b):
     switch = 1
-    if len(a) == len(b):
-        for letter in range(len(a)):
-            if a[letter] != b[letter]:
-                switch = 0
+    if len(a) == len(b) and sorted(a) == sorted(b):
+        switch = 1
     else:
         switch = 0
     return switch
 
-str1 = sorted(input("Enter first string: "))
-str2 = sorted(input("Enter second string: "))
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
 
 print("True") if check(str1, str2) == 1 else print("False")
