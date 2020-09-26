2541823fc6e6067895c19f39e0dfdfc2ab6416cd
Jatin Agrawal <47699187+JatinAgrawal94@users.noreply.github.com>
2020-09-26 18:37:33 +0530

    Update HCF.py

--- Maths/HCF.py
+++ Maths/HCF.py
@@ -3,22 +3,6 @@
 def calcHCF(a,b):
     return b if a%b==0 else calcHCF(b, a%b)
 
-# Function for finding fibonacci series upto any number
-def fibonacci(n):
-    if(n>20):
-        return
-    result=[]
-    a=0
-    b=0
-    c=1
-    while(n>0):
-        result.append(a)
-        a=b+c
-        c=b
-        b=a
-        n-=1
-    
-
 if __name__ == '__main__':
     
     n = int(input())
@@ -29,4 +13,3 @@ if __name__ == '__main__':
         hcf = calcHCF(hcf, arr[i])
     
     print(hcf)
-    print(fibonacci(20))
