5d79f075c42c3772491d7d26f2929691c82bcdc0
Jatin Agrawal <47699187+JatinAgrawal94@users.noreply.github.com>
2020-09-26 18:28:02 +0530

    Update HCF.py
    Function to find fibonacci serires upto 20

--- Maths/HCF.py
+++ Maths/HCF.py
@@ -1,5 +1,23 @@
+
+# function to find hcf of given numbers
 def calcHCF(a,b):
     return b if a%b==0 else calcHCF(b, a%b)
+
+# Function for finding fibonacci series upto any number
+def fibonacci(n):
+    if(n>20):
+        return
+    result=[]
+    a=0
+    b=0
+    c=1
+    while(n>0):
+        result.append(a)
+        a=b+c
+        c=b
+        b=a
+        n-=1
+    
 
 if __name__ == '__main__':
     
