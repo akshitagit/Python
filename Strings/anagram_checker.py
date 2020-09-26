cd9b5973d6bec40029b4be7b2a6469bfe2660d20
Adel Bordbari <blackdead263@yahoo.com>
2020-09-26 06:44:40 +0330

    add Strings/anagram_checker.py

--- Strings/anagram_checker.py
+++ Strings/anagram_checker.py
@@ -1,12 +1,14 @@
-def check(s1, s2): 
-      
-    # the sorted strings are checked  
-    if(sorted(s1)== sorted(s2)): 
-        print("The strings are anagrams.")  
-    else: 
-        print("The strings aren't anagrams.")          
-          
-# driver code   
-s1 = input("enter string 1: ")
-s2 = input("enter string 2: ")
-check(s1, s2)
+def check(a, b):
+    switch = 1
+    if len(a) == len(b):
+        for letter in range(len(a)):
+            if a[letter] != b[letter]:
+                switch = 0
+    else:
+        switch = 0
+    return switch
+
+str1 = sorted(input("Enter first string: "))
+str2 = sorted(input("Enter second string: "))
+
+print("True") if check(str1, str2) == 1 else print("False")
