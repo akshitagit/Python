8807f8791a901f6ef1f8527cc3e5776bf00665f2
HimadriPathak <pathak.himadri7458@gmail.com>
2020-09-30 12:56:01 +0530

    a better program with less complexity

--- Array/Rotate_Left.py
+++ Array/Rotate_Left.py
@@ -4,37 +4,20 @@
 # In[ ]:
 
 
-
-
 arr = list();     
 n=input("Enter the number of elements you want in the array: ")    
 
-print("Enter the array elements one by one:")
+#print("Enter the array elements one by one:")
 for i in range(int(n)):
-    ele=input()
-    arr.append(int(ele))
-     
+    print("Enter the",i+1,"element of the array: ",end='')
+    arr.append(int(input()))
+
 #Display the original array    
-print("Array before rotation is: ")   
-for i in range(0, len(arr)):    
-    print(arr[i])
-     
-#Rotate the given array by 3 times towards left    
-for i in range(0, 3):    
-    #Stores the first element of the array    
-    first = arr[0]
-        
-    for j in range(0, len(arr)-1):    
-       #Shift element of array by one    
-          arr[j] = arr[j+1]
-            
-    #First element of array will be added to the end    
-    arr[len(arr)-1] = first   
-     
-print()  
-     
-#Display the array after 3 left rotations   
-print("Array after 3 left rotations is: ");    
-for i in range(0, len(arr)):    
-    print(arr[i])
+print("Array before rotation is:",arr)   
+
+#rotation of the array by 3 elements to left by slicing
+l=arr[0:3]
+del arr[0:3]
+arr=arr+l
+print('Array after rotation is:',arr)
 
