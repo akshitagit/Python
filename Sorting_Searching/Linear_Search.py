d9e5f2318937fc776dd585d746b98fc98d5acecd
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-10-01 08:23:49 +0530

    Reducing the Space Complixity

--- Sorting_Searching/Linear_Search.py
+++ Sorting_Searching/Linear_Search.py
@@ -3,7 +3,7 @@ global size
 def linearSearch(arr,find):
     for i in range(size):
         if arr[i] == find:
-            print("Element is found at " + str(i) + "Index\n")
+            print("Element is found at " + str(i) + " Index\n")
             print("\n Note: - Element start from 0 index")
             break
     else:
@@ -15,11 +15,11 @@ if __name__ == "__main__":
     size = int(input("Enter a size of the array\n"))
 
     array = []
-    print("Enter the element of the array\n")
+    print("Enter the element of the array")
 
     for i in range(size):
-        temp = int(input())
-        array.append(temp)
+        # temp = 
+        array.append(int(input()))
 
-    search = int(input("Enter a element to be searched\n"))
-    linearSearch(array, search)    
+    search = int(input("Enter a element to be searched\t"))
+    linearSearch(array, search)
