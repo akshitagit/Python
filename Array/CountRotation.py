4fecfe1303d3f3c970aea5d1b21c3a4f0c981dbb
Shruti Sharma <56972234+sj056@users.noreply.github.com>
2020-10-02 23:17:01 +0530

    Create CountRotation.py

---
+++ Array/CountRotation.py
@@ -0,0 +1,11 @@
+def countRotations(arr, n): 
+    min = arr[0] 
+    for i in range(0, n): 
+        if (min > arr[i]): 
+            min = arr[i] 
+            min_index = i 
+    return min_index; 
+  
+arr = [15, 18, 2, 3, 6, 12] 
+n = len(arr) 
+print(countRotations(arr, n))
