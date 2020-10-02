1bcd77351a5e9147b9a3a38af51c760f13f74b6f
Avirat Gupta <55698467+AviratGupta@users.noreply.github.com>
2020-10-02 15:33:35 +0530

    Update reverse_string.py

--- Strings/reverse_string.py
+++ Strings/reverse_string.py
@@ -1,10 +1,2 @@
-#take input from the user
-input_str =  input("Enter the string: ")
-
-#defiene the function
-def rev_string(string):
-	string = string[::-1]
-	return string
-
-#call the reverse function
-print(rev_string(input_str))
+str = input() # initial string
+print(str[len(input())::-1]) # slicing 
