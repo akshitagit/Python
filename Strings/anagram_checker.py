73eba8893971b69ad7ffb59071413c94477d9178
Avirat Gupta <55698467+AviratGupta@users.noreply.github.com>
2020-10-05 16:47:58 +0530

    Update anagram_checker.py

--- Strings/anagram_checker.py
+++ Strings/anagram_checker.py
@@ -1,3 +1,26 @@
+CHARS = 26
+t = int(input())
+for i in range(t):
+    def remAnagram(a, b): 
+        count1 = [0]*CHARS 
+        count2 = [0]*CHARS 
+        i = 0
+        while i < len(a): 
+            count1[ord(a[i])-ord('a')] += 1
+            i += 1
+        i =0
+        while i < len(b): 
+            count2[ord(b[i])-ord('a')] += 1
+            i += 1
+        result = 0
+        for i in range(26): 
+            result += abs(count1[i] - count2[i]) 
+        return result 
+    if __name__ == "__main__": 
+        a = str(input())
+        b = str(input())
+        print(remAnagram(a, b)) 
+'''
 def check(s1, s2): 
       
     # the sorted strings are checked  
@@ -10,3 +33,4 @@ def check(s1, s2):
 s1 = input("enter string 1: ")
 s2 = input("enter string 2: ")
 check(s1, s2)
+'''
